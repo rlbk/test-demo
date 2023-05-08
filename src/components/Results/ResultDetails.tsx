65e8ffd285ed62d341069b9811a78e81ca0ce827
JobAxle <rlbishwokarma@gmail.com>
2023-05-08 13:29:31 +0545

    final code

--- src/components/Results/ResultDetails.tsx
+++ src/components/Results/ResultDetails.tsx
@@ -1,8 +1,8 @@
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 
 const ResultDetails = ({ imageSrc, searchKeyword, searchLocation }: { imageSrc: string, searchKeyword: string, searchLocation: string }): ReactElement => {
     return (
-        <div className='mb-4 sm:mb-0 flex items-center'>{imageSrc ? <img src={imageSrc} alt="result review" /> : <div className=' w-28 sm:h-24 sm:w-24 bg-slate-500'></div>}
+        <div className='mb-4 sm:mb-0 flex items-center'>{imageSrc ? <img src={imageSrc} alt="result review" /> : <div className=' w-28 h-24 sm:w-24 bg-slate-500'></div>}
             <div className='ml-2'><p >{searchKeyword}</p>
                 <p className='text-sm text-slate-500'>{searchLocation}</p>
             </div>
