15bfb34d105c2783345382aa247b484128251f66
JobAxle <rlbishwokarma@gmail.com>
2023-05-08 13:34:10 +0545

    final code

--- src/components/Results/ResultModal.tsx
+++ src/components/Results/ResultModal.tsx
@@ -4,7 +4,7 @@ import ResultEnquiry from "./ResultEnquiry"
 
 const ResultModal = ({ searchedKeyword, searchedLocation }: { searchedKeyword: string, searchedLocation: string }): ReactElement => {
     return (
-        <div className="mx-[auto] sm:mx-0 w-[60%] sm:w-[80%] my-4 flex flex-col sm:flex-row justify-between items-center p-4 sm:p-6 shadow-md rounded    shadow-neutral-500/50">
+        <div className="mx-[auto] sm:mx-0 w-[60%] sm:w-[80%] my-4 flex flex-col sm:flex-row justify-between sm:items-center py-4 px-10 sm:p-6 shadow-md rounded    shadow-neutral-500/50">
             <ResultDetails imageSrc="" searchKeyword={searchedKeyword} searchLocation={searchedLocation} />
             <ResultEnquiry />
         </div>
