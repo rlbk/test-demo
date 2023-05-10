10ef7fe31639f1781a4369aaaf8d9d5d2e50589e
JobAxle <rlbishwokarma@gmail.com>
2023-05-10 09:16:47 +0545

    final code : icons typo refactor

--- src/components/Common/BlueModal.tsx
+++ src/components/Common/BlueModal.tsx
@@ -1,12 +1,11 @@
 import { ReactElement } from 'react'
-import { FaHandshake } from 'react-icons/fa'
-import { FiMessageSquare } from 'react-icons/fi'
+import { IconType } from 'react-icons'
 
 
-const BlueModal = ({ icons, title }: { icons: string, title: string }): ReactElement => {
+const BlueModal = ({ Icons, title, }: { Icons: IconType, title: string, }): ReactElement => {
     return (
         <div className="bg-sky-500  hover:bg-sky-600 cursor-pointer  text-white text-xs sm:text-sm flex flex-row justify-between items-center mb-2 sm:mb-0 px-2 py-1 rounded-md">
-            {icons == 'message' ? <FiMessageSquare size={26} /> : <FaHandshake size={30} />}
+            <Icons size={26} />
             <p className='ml-2 sm:ml-3 capitalize'>{title}</p>
         </div>
     )
--- src/components/Results/ResultEnquiry.tsx
+++ src/components/Results/ResultEnquiry.tsx
@@ -1,11 +1,17 @@
 import { ReactElement } from 'react'
 import { BlueModal } from '../Common'
+import { FaHandshake } from 'react-icons/fa'
+import { FiMessageSquare } from 'react-icons/fi'
+
+
 
 const ResultEnquiry = (): ReactElement => {
     return (
         <div className='flex sm:h-24  flex-col justify-around sm:justify-between  items-start'>
-            <BlueModal icons='message' title='Send Message' />
-            <BlueModal icons='hands' title='Request Collabration' />
+            <BlueModal Icons={FiMessageSquare} title='Send Message' />
+            <BlueModal Icons={FaHandshake} title='Request Collabration' />
+
+
         </div>
     )
 }
